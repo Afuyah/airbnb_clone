473073675b16e4928f06f818a17fcd16e1c2f3f6
Bha <bha@Jhsync254.afuya.co>
2024-10-07 04:31:29 +0300

    minor corrections

--- app/models/models.py
+++ app/models/models.py
@@ -15,7 +15,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     role = db.Column(db.String(50), nullable=True)
 
     # Specify foreign_keys for the owner-user relationship
